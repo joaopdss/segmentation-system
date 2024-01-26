565191e87c204f1c70123c661351c07738c40959
joaop_dss <dev.joaopedrods@outlook.com>
2024-01-25 22:32:04 -0300

    starting to convert onehot to rgb

--- src/test.py
+++ src/test.py
@@ -2,13 +2,21 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
-model = tf.keras.models.load_model("../models/unet.h5")
+def onehot_to_rgb(one_hot, color_dict):
+	one_hot = np.array(one_hot)
+	one_hot = np.argmax(one_hot, axis=-1)
+	output = np.zeros(one_hot.shape[1:3]+(3,))
+
+	print(one_hot.shape)
+
+model = tf.keras.models.load_model(r"C:\Users\devjo\Documents\Projects\semantic-segmentation\models\unet.h5")
 
 img = tf.io.read_file("../imgs/_J6Ge9plcquFpJng9IXIAQ.jpg")
 img = tf.io.decode_png(img)
 img = tf.image.convert_image_dtype(img, tf.float16)
 
 preds = model.predict(tf.expand_dims(img, axis=0))
-print(preds[0])
-print(np.array(preds[0][150][150]).astype(np.int32))
+
+onehot_to_rgb(preds, 0)
+
 
